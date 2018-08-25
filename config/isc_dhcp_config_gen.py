0cf24fa5e9507d54315b30f9986032aac6306d5a
Nicolai Brogaard <nmb@minserver.dk>
2018-08-25 11:15:12 +0200

    serverfication

--- config/isc_dhcp_config_gen.py
+++ config/isc_dhcp_config_gen.py
@@ -5,7 +5,7 @@ import os
 import urllib.request
 import io
 
-netbox = 'https://netbox.minserver.dk/ipam/prefixes/?status=1&parent=&family=&q=&vrf=npflan&mask_length=&export'
+netbox = 'https://netbox.minserver.dk/ipam/prefixes/?q=&within_include=&family=&mask_length=&vrf=npflan&status=1&role=server-net-dhcp&export'
 data = urllib.request.urlopen(netbox).read()
 
 datafile = os.path.join(os.path.dirname(__file__), 'data.csv')
@@ -17,7 +17,7 @@ reader = csv.reader(io.StringIO(data.decode()), delimiter=',', quotechar='|')
 print('default-lease-time 7200;')
 print('max-lease-time 28800;\n')
 print('authoritative;\n')
-print('option domain-name "npf";\n')
+print('option domain-name "server.npf";\n')
 print('option domain-name-servers 10.96.5.1;\n')
 
 print('omapi-port 7911;')
@@ -28,16 +28,6 @@ print('\t algorithm hmac-md5;')
 print('\t secret LsMkrX1xprEQQBoeGAC6NvokcA/mcuD5LnBqD/2gxJEpZqTTRN7vtPm0BVW03zl0oo1qWCVrNTqsoAq2B8hrsg==;')
 print('};\n')
 
-# Argument to generate include for high availability config
-if len(sys.argv) > 1 and sys.argv[1] == "master":
-    print('include "/etc/dhcpd.master";\n')
-elif len(sys.argv) > 1 and sys.argv[1] == "slave":
-    print('include "/etc/dhcpd.slave";\n')
-else:
-    print('include "/dhcp/failover/dhcpd.failover";\n')
-
-print('subnet 10.90.0.0 netmask 255.255.0.0 {')
-print('}\n')
 
 for row in reader:
     if row[7].lower() == "Access".lower() or row[7].lower() == "Wireless".lower() or row[9].lower() == "AP-MGMT".lower():
@@ -51,7 +41,6 @@ for row in reader:
         print("subnet " + network + " netmask " + subnetmask + " {")
         print("\t pool {")
         print("\t\t range " + str(ip[4]) + " " + str(ip[pow(2, (32-ip.prefixlen))-6]) + ";")
-        print('\t\t failover peer "failover-partner";')
         print("\t\t option routers " + str(ip[1]) + ";")
         print("\t}")
         print("}\n")
