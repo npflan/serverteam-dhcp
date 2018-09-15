130a18f2f455afc875f74bb8700e1c9551c9158d
Nicolai Brogaard <nmb@minserver.dk>
2018-09-15 13:09:31 +0200

    allow booting and fixed deployment yaml

--- config/isc_dhcp_config_gen.py
+++ config/isc_dhcp_config_gen.py
@@ -17,6 +17,9 @@ reader = csv.DictReader(io.StringIO(data.decode()), delimiter=',', quotechar='|'
 print('default-lease-time 7200;\n')
 print('max-lease-time 28800;\n')
 print('authoritative;\n')
+
+print('allow booting;\n')
+
 print('option domain-name "server.npf";\n')
 print('option domain-name-servers 10.101.128.128;\n')
 
@@ -24,7 +27,6 @@ print('option domain-name-servers 10.101.128.128;\n')
 print('ddns-updates on;\n')
 print('ddns-update-style interim;\n')
 
-print('ddns-update-style interim;\n')
 print('''
 zone rack1.server.npf. {
   primary 10.101.128.128;
@@ -48,6 +50,10 @@ zone 103.100.10.in-addr.arpa. {
 print('subnet 10.101.1.0 netmask 255.255.255.0 {}')
 print('subnet 10.101.2.0 netmask 255.255.255.0 {}')
 print('subnet 10.101.3.0 netmask 255.255.255.0 {}')
+print('subnet 10.101.4.0 netmask 255.255.255.0 {}')
+print('subnet 10.101.5.0 netmask 255.255.255.0 {}')
+print('subnet 10.101.6.0 netmask 255.255.255.0 {}')
+print('subnet 10.101.7.0 netmask 255.255.255.0 {}')
 print('subnet 10.100.128.0 netmask 255.255.192.0 {}')
 
 
@@ -57,7 +63,6 @@ for row in reader:
         ip = ipaddress.IPv4Network(row['prefix'])
     except ipaddress.AddressValueError:
         print(row['prefix'] + " is not a valid ip",file=sys.stderr)
-
     parts = ip.with_netmask.split('/')
     network = parts[0]
     subnetmask = parts[1]
@@ -66,5 +71,7 @@ for row in reader:
     print("\t\t range " + str(ip[50]) + " " + str(ip[pow(2, (32-ip.prefixlen))-50]) + ";")
     print("\t\t option routers " + str(ip[1]) + ";")
     print('\t\t option domain-name "'+row['description']+'";\n')
+    print('\t\t next-server "serverteam-pxe.default.k8s.server.npf";\n')
+    print('\t\t filename "pxelinux.0";\n')
     print("\t}")
     print("}\n")
